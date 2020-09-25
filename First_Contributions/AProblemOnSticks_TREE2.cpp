3ecee600d15e60b2e0109d1a0af0d82dd19cc8cb
Rajarshi <acharjeerishi99@gmail.com>
2020-09-25 10:37:29 +0530

    AProblemOnSticks_TREE2 solved

---
+++ First_Contributions/AProblemOnSticks_TREE2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ll test;
+    cin >> test;
+    while (test--)
+    {
+        ll n, temp, count = 0;
+        cin >> n;
+        unordered_map<ll, ll> m;
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> temp;
+            m[temp]++;
+        }
+        for (auto it = m.begin(); it != m.end(); it++)
+        {
+            if (it->first != 0)
+            {
+                count++;
+            }
+        }
+        cout << count << "\n";
+    }
+    return 0;
+}
