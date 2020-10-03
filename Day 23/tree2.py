a53b143b08efdb49ec3ccd1826c8f766658fa829
Aditi Jha <62383312+aditijha1302@users.noreply.github.com>
2020-10-03 10:36:00 +0530

    September Long challenge

---
+++ Day 23/tree2.py
@@ -0,0 +1,10 @@
+def main():
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        h=list(map(int, input().split()[:n]))
+        s=set(h)
+        s.discard(0)
+        print(len(s))
+    return 0
+main()
