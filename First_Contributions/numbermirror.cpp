1159f7a5c883358dad78e56d03701fbf1510b07c
mgncodes <61737202+mgncodes@users.noreply.github.com>
2020-10-08 18:39:13 +0530

    add solution for numbermirror (START01)
    solves issue #67

---
+++ First_Contributions/numbermirror.cpp
@@ -0,0 +1,14 @@
+//numbermirror codechef solution by mgncodes
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n; cin >> n;
+    cout << n << "\n";
+    return 0;
+}
